f18b0d9c2908d97cbc04739ff6a72bb2a3dcad87
Artem <moranmr8@gmail.com>
2019-01-28 16:00:00 +0300

    Добавлены заглушки для методов

--- site/Controllers/MainController.cs
+++ site/Controllers/MainController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using site.Models;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
@@ -17,11 +18,117 @@ namespace site.Controllers
 			optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=usersstoredb;Trusted_Connection=True;MultipleActiveResultSets=true");
 			db = new ApplicationContext(optionsBuilder.Options);
 		}
+		
+		[Route("GetArticles")]
+		[HttpGet]
+		public JsonResult GetArticles(int? skip, int? limit)
+		{
+			var temp = new
+			{
+				skip,
+				limit
+			};
+			return Json(temp);
 
+		}
+
+
+		[Route("GetTopArticles")]
+		[HttpGet]
+		public JsonResult GetTopArticles()
+		{
+			var temp = new
+			{
+				kek = new string[] {"тут будут топовые статьи"}
+			};
+
+			return Json(temp);
+		}
+
+		[Route("GetArticle")]
+		[HttpGet]
+		public JsonResult GetArticle(int? id)
+		{
+			var temp = new
+			{
+				id
+			};
+			return Json(temp);
+		}
+
+		[Route("GetTeams")]
+		[HttpGet]
+		public JsonResult GetTeams()
+		{
+			var temp = new
+			{
+				k = "k"
+			};
+			return Json(temp);
+		}
+
+		[Route("GetTeamInfo")]
+		[HttpGet]
+		public JsonResult GetTeamInfo(int? id)
+		{
+			var temp = new
+			{
+				teamId = id
+			};
+			return Json(temp);
+		}
+
+		[Route("GetMembers")]
+		[HttpGet]
+		public JsonResult GetMembers(string id)
+		{
+			var temp = new
+			{
+				hui = "hui"
+			};
+			if (id != null)
+			{
+				if (id.ToLower() == "all")
+					return Json(temp);
+				if (id.All(char.IsDigit))
+				{
+					int _id = Convert.ToInt32(id);
+					return Json(temp);
+				}
+			}
+			throw new NullReferenceException();
+		}
+
+		[Route("GetMembers")]
+		[HttpGet]
+		public JsonResult GetMembers(string id, int? offset, int? count)
+		{
+			var temp = new
+			{
+				offset,
+				count
+			};
+			return Json(temp);
+		}
+		
+		[Route("GetMemberInfo")]
+		[HttpGet]
+		public JsonResult GetMemberInfo(string id)
+		{
+			if (id.All(char.IsDigit))
+			{
+				int _id = Convert.ToInt32(id);
+			}
+			var temp = new
+			{
+				id
+			};
+			return Json(temp);
+		}
+		
 		[Route("GetProjects")]
 		public JsonResult GetProjects()
 		{
-			
 			List<Project> projects = new List<Project>();
 
 			projects.Add(new Project { Name = "Проект 1", Description = "Супер проект", Img = "https://www.voltmobi.com/wp-content/uploads/Untitled-5.jpg" });
@@ -31,22 +138,14 @@ namespace site.Controllers
 			return Json(projects);
 		}
 
-		[Route("GetTeams")]
-		public JsonResult GetTeams()
-		{
-			List<Team> teams = new List<Team>();
-
-			teams.Add(new Team { Name = "Команда 1", Description = "Супер команда", Members = new List<User> { new User { UserName = "Ваня", photo = "https://www.internet-design.ru/upload/iblock/4ea/programmist.svg" } } });
-			teams.Add(new Team { Name = "Команда 2", Description = "Супер команда", Members = new List<User> { new User { UserName = "Петя", photo = "https://www.internet-design.ru/upload/iblock/4ea/programmist.svg" } } });
-			teams.Add(new Team { Name = "Команда 3", Description = "Супер команда", Members = new List<User> { new User { UserName = "Коля", photo = "https://www.internet-design.ru/upload/iblock/4ea/programmist.svg" } } });
-
-			return Json(teams);
-		}
+	
 
 		[Route("GetTeam")]
 		public JsonResult GetTeam(Project p)
 		{	
 			return Json(p);
 		}
+		
+		
 	}
 }
