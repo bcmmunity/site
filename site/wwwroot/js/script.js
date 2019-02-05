ba2daec92bc1920bdf3dfb8c981aa8b0695f99ef
Pewpaf <moranmr8@gmail.com>
2019-02-05 23:25:22 +0300

    Страница переверстана, добавлен новый шаблон

---
+++ site/wwwroot/js/script.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  let lang_icon = document.getElementById('lang_icon').contentDocument;
+  let lang_elem = lang_icon.getElementById("Capa_1");
+  lang_elem.style.fill = "#434343";
+
+  let join_icons = document.getElementsByClassName('join__img');
+  let icons_arr = Array.from(join_icons);
+  let icon_elem;
+  icons_arr.forEach(function(elem) {
+    icon_elem  = elem.contentDocument.getElementById("Capa_1");
+    icon_elem.style.fill = "#434343";
+  });
+
+  let team_select = document.getElementsByClassName('team__selection')[0].children;
+  let kek = Array.from(team_select);
+  kek.forEach(function(elem) {
+    elem.addEventListener("click", function(event) {
+      event.preventDefault();
+    })
+  });
+}
