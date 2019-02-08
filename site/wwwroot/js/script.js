0a5f360f80a0a0acb47560912b7eff6f62baeefa
Pewpaf <moranmr8@gmail.com>
2019-02-08 22:37:53 +0300

    Незначительные фиксы

--- site/wwwroot/js/script.js
+++ site/wwwroot/js/script.js
@@ -21,10 +21,13 @@ $(function() {
   $(".hamburger").on("click", function(e) {
     var l = $(this).attr("data-active");
     if (l === "false") {
+
+      $(".overlay").css( "zIndex", 5 );
       $(".overlay").toggleClass("overlay__active");
       $(this).toggleClass("hamburger__active");
       $(this).attr("data-active", "true");
     } else {
+      $(".overlay").css( "zIndex", -5 );
       $(".overlay").toggleClass("overlay__active");
       $(this).toggleClass("hamburger__active");
       $(this).attr("data-active", "false");
